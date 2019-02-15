35d7e403b956e8fd943e7e81570883cdabf25d6e
zhanghaiyu <zhanghaiyu@reignwood.com>
2019-02-15 17:59:04 +0800

    搭建docker eureka服务

--- product-center/product-cloud/src/main/java/org/product/controller/sku/PcSkuTypeController.java
+++ product-center/product-cloud/src/main/java/org/product/controller/sku/PcSkuTypeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/product/sku/status/")
+@RequestMapping("/product/sku/type/")
 public class PcSkuTypeController extends BaseController<PcSkuType, IPcSkuTypeService> {
 
 }
