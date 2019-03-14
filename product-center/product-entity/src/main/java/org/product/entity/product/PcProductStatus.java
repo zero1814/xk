b363ae1580a67dde8048b3d07a41a89ba5eacf52
zhanghaiyu <zhanghaiyu@reignwood.com>
2019-03-14 14:40:25 +0800

    zhy

--- product-center/product-entity/src/main/java/org/product/entity/product/PcProductStatus.java
+++ product-center/product-entity/src/main/java/org/product/entity/product/PcProductStatus.java
@@ -7,7 +7,7 @@ import javax.persistence.Table;
 import org.zero.spring.jpa.BaseEntity;
 
 @Entity
-@Table(name="pc_product_status")
+@Table(name = "pc_product_status")
 public class PcProductStatus extends BaseEntity {
 
 	private static final long serialVersionUID = -6330335877354066179L;
@@ -15,9 +15,14 @@ public class PcProductStatus extends BaseEntity {
 	/**
 	 * 名称
 	 */
-	@Column(name="name",length=50,nullable=false)
+	@Column(name = "name", length = 50, nullable = false)
 	private String name;
-	
+
+	/**
+	 * 是否可用 0 可用 1 不可用
+	 */
+	@Column(name = "flag_enabled", columnDefinition = "int default 0")
+	private Integer flagEnabled;
 
 	public String getName() {
 		return name;
@@ -26,4 +31,13 @@ public class PcProductStatus extends BaseEntity {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public Integer getFlagEnabled() {
+		return flagEnabled;
+	}
+
+	public void setFlagEnabled(Integer flagEnabled) {
+		this.flagEnabled = flagEnabled;
+	}
+
 }
