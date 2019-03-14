f448b5f716a93ac0c703f17ec8f28e9dc016aa2a
zhanghaiyu <zhanghaiyu@reignwood.com>
2019-03-14 10:57:39 +0800

    zhy

--- product-center/product-service/src/main/java/org/product/service/product/IPcProductPicService.java
+++
@@ -1,8 +0,0 @@
-package org.product.service.product;
-
-import org.product.entity.product.PcProductPic;
-import org.zero.spring.jpa.IBaseService;
-
-public interface IPcProductPicService extends IBaseService<PcProductPic, String> {
-
-}
