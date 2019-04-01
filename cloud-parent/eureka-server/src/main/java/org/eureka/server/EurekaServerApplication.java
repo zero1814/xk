119150d48115c383151e272a52496344fe6744b8
zhanghaiyu <zhanghaiyu@reignwood.com>
2019-04-01 10:51:04 +0800

    添加zuul gateway

---
+++ cloud-parent/eureka-server/src/main/java/org/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package org.eureka.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class EurekaServerApplication 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ cloud-parent/zuul-gateway-server/src/main/java/org/zuul/gateway/server/App.java
@@ -0,0 +1,13 @@
+package org.zuul.gateway.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
