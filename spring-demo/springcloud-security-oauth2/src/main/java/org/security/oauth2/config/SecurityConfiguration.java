5a481cda87fe6315716299eb4e4a765303c53d12
zhanghaiyu <zhanghaiyu@reignwood.com>
2019-07-09 08:57:13 +0800

    zhy

--- spring-demo/springcloud-security-oauth2/src/main/java/org/security/oauth2/config/SecurityConfiguration.java
+++ spring-demo/springcloud-security-oauth2/src/main/java/org/security/oauth2/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Bean
 	@Override
--- spring-demo/springcloud-security-oauth2/src/main/java/org/security/oauth2/controller/TestController.java
+++ spring-demo/springcloud-security-oauth2/src/main/java/org/security/oauth2/controller/TestController.java
@@ -6,18 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.extern.slf4j.Slf4j;
-
 @RestController
 @RequestMapping("/user/")
-@Slf4j
 public class TestController {
 
 	@RequestMapping(value = "/current", method = RequestMethod.GET)
 	public Principal getUser(Principal principal) {
-		log.info(">>>>>>>>>>>>>>>>>>>>>>>>");
-		log.info(principal.toString());
-		log.info(">>>>>>>>>>>>>>>>>>>>>>>>");
 		return principal;
 	}
 }
